29214e1e2a5e25f1ed08093ee3334588dc2deac8
yogalakshmi123 <e0223027@sriher.edu.in>
2026-04-21 09:00:47 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default({
+  server:{
+    "host":true ,
+    "port":5173,
+    "open":true
+  }
+});
